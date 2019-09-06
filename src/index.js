4f15ca072a6befd23472c3d13eef17d4542c69aa
DaeHun <skypedanny@naver.com>
2019-09-06 12:10:57 +0900

    koa backend

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const Koa = require('koa')
+const app = new Koa()
+const bodyParser = require('koa-body')
+const mongoose = require('mongoose')
+
+const tasks = require('./routes/tasks')
+
+app.use(bodyParser())
+app.use(tasks.routes())
+
+mongoose.connect(
+    'mongodb://localhost/nodejstasks',
+    { useNewUrlParser: true }
+)
+
+app.listen(5000, () => {
+    console.log('Server running at port 5000');
+})
